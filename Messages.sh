3e0bb9d5f5333fff40e177a0dcc67cacefb85209
Camilo Higuita <chiguitar@unal.edu.co>
2022-11-10 18:39:11 -0500

    fix translations by using the right domain name

--- Messages.sh
+++ Messages.sh
@@ -6,4 +6,4 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
-$XGETTEXT $(find src/ -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/MauiKitTextEditorPlugin.pot
+$XGETTEXT $(find src/ -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/mauikittexteditor.pot
